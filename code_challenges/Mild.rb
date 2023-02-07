16a48c9db3a377e09f02f084aa5fa20cd8182455
Justin Pasko <jpasko1112@gmail.com>
2023-02-07 13:45:11 -0500

    Add ruby integer odd? and ruby integer even? commands

--- code_challenges/Mild.rb
+++ code_challenges/Mild.rb
@@ -2,10 +2,10 @@
 
 # Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
 
-num = 7
+num = 13
 
-if num == 7
-    p "Odd"
-else num == 2
-    p "Even"
+if num.odd?
+    puts "odd"
+else num.even?
+    puts "even"
 end
