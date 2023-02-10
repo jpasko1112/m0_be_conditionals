4b67cf38351867a3ce81476b65af3d19eb78bd2b
Justin Pasko <jpasko1112@gmail.com>
2023-02-10 17:35:12 -0500

    Simplify code block.

--- code_challenges/Mild.rb
+++ code_challenges/Mild.rb
@@ -2,10 +2,10 @@
 
 # Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
 
-num = 13
+num = 14
 
 if num.odd?
     puts "odd"
-else num.even?
+else
     puts "even"
 end
